71b8d12d106b654bbb6e02a8e2b20a8f1646dc92
Martijn Jacobs <maerteijn@gmail.com>
2021-11-26 14:15:41 +0100

    Render the main element with js in the demo app

--- demo/main.js
+++ demo/main.js
@@ -1,8 +1,18 @@
 import Caldera from "../src/caldera.js"
 import HelloWorld from "./js/components/helloworld.js"
 
+import Overview from "./js/pages/overview.js"
+
 Caldera.registerComponent("hello-world", HelloWorld)
+Caldera.registerComponent("overview-page", Overview)
 Caldera.setInitalState({ name: "My Name" })
+
+
+// TODO add the simple router here from https://github.com/praveen-me/simple-vanila-router/blob/master/app.js
+// to select the right page
+const appElement = document.querySelector("div#app")
+appElement.innerHTML = `<hello-world state-id="name"></hello-world>`
+
 
 // these are here for debugging purposes
 window.caldera = Caldera
