291efee04795364ecb22aea428b127538520bdc9
Martijn Jacobs <maerteijn@gmail.com>
2021-12-16 15:36:24 +0100

    Extended the component unittest

--- tests/test_component.js
+++ tests/test_component.js
@@ -15,6 +15,20 @@ Caldera.registerComponent(AnotherComponent)
 Caldera.setInitalState({ name: "My Name" })
 
 describe("Test the AnotherComponent", () => {
+  it("We can create a new instance of AnotherComponent with props", () => {
+    const another = new AnotherComponent({ name: "My Name" })
+    assert.equal(another.props.name, "My Name")
+  })
+
+  it("We can just create a new instance of AnotherComponent and insert it", () => {
+    const another = new AnotherComponent()
+    document.body.appendChild(another)
+
+    // so the default state name is 'My Name' and this should be rendered in the div
+    const div = document.body.querySelector("div")
+    assert.equal(div.innerHTML, "My Name")
+  })
+
   it("The AnotherComponent component renders the name from the state", () => {
     document.body.innerHTML = `<another-component></another-component>`
 
